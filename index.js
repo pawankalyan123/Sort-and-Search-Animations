642b0bd3efeb68a3e0cd67e24e3ceeb6348f425a
Venkateswara reddy Chaikam <84827565+pawankalyan123@users.noreply.github.com>
2023-05-16 13:03:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const app=express();
 const ejs=require('ejs');
-const PORT=process.env.PORT;
+const PORT=process.env.PORT || 3000;
 app.set('view engine','ejs');
 app.use('/public',express.static('public'));
 
